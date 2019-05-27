810c25f2628409a37f6315016acdf382c50e5ce0
rjOwl <11742610+rjOwl@users.noreply.github.com>
2019-05-27 10:56:07 +0200

    First Version

---
+++ README.md
@@ -0,0 +1,4 @@
+Install greasemonkey then either 
+- create a new user script then copy and paste code into it
+- import hide_ads.js inside the extension
+
---
+++ hide_ads.js
@@ -0,0 +1,23 @@
+// ==UserScript== 
+// @name kissanime_No_ads 
+// @version 1 
+// @author Le mofasa 
+// @grant none 
+// ==/UserScript== 
+var url="https://kissanime.ru/"; 
+if(window.location.href.includes(url)){ 
+	function removeElement(elementId) { 
+		// Removes an element from the document 
+		var element = document.getElementById(elementId); 
+		element.parentNode.removeChild(element); 
+	} 
+	var ids=["divAds", "adsIfrme1", "adsIfrme3", "divAds2", "divAds", "center", "adsIfrme6", "adsIfrme7", "divFloatLeft"] 
+	idsLen = ids.length; 
+	for (i = 0; i < idsLen; i++) { 
+		try{ 
+			removeElement(ids[i]) 
+		} 
+		catch{ ; 
+		} 
+	} 
+}
